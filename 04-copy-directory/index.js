5fb3c5d0601ed274421b117ad5d88f8d6a8572b2
Dmitry Hlebin <3708576@gmail.com>
2022-05-22 23:54:38 +0300

    feat: 4 task comlete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+const filePath = path.join(__dirname, 'files-copy');
+const fileOutPath = path.join(__dirname, 'files');
 
+if (fs.existsSync(filePath)){
+    fs.readdir(filePath, (err,files) => {
+        if(err){throw err}
+              files.forEach(file => {
+                    fs.unlink(path.join(filePath, file), ()=>{})              
+                    })
+            });
+     fs.rmdir(filePath, ()=>{})
+};
+path.join(fileOutPath, 'test-text.txt');
+
+fs.mkdir(filePath, {options: false}, () => {})//create folder
+
+fs.readdir(fileOutPath, (error, files) => {
+      files.forEach(file => {
+            fs.copyFile(path.join(fileOutPath, file),path.join(filePath, file),() => {})
+      
+            })
+    });
